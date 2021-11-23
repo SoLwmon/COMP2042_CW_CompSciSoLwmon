2d4a8bd1ebd8010e1643e16440cc668129751d3c
SoLwmon <83406615+SoLwmon@users.noreply.github.com>
2021-11-24 00:16:56 +0800

    Update ClayBrick.java

--- Brick_Destroy-master/Brick_Destroy-master/src/test/ClayBrick.java
+++ Brick_Destroy-master/Brick_Destroy-master/src/test/ClayBrick.java
@@ -1,8 +1,6 @@
 package test;
 
 import java.awt.*;
-import java.awt.Point;
-import java.awt.geom.Point2D;
 
 
 /**
@@ -15,11 +13,6 @@ public class ClayBrick extends Brick {
     private static final Color DEF_INNER = new Color(178, 34, 34).darker();
     private static final Color DEF_BORDER = Color.GRAY;
     private static final int CLAY_STRENGTH = 1;
-
-
-
-
-
 
     public ClayBrick(Point point, Dimension size){
         super(NAME,point,size,DEF_BORDER,DEF_INNER,CLAY_STRENGTH);
