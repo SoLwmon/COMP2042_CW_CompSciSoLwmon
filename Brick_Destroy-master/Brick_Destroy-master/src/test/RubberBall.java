cc87a8437f393eea267d9f35679eaf172f8b459f
SoLwmon <83406615+SoLwmon@users.noreply.github.com>
2021-11-24 00:24:57 +0800

    Update RubberBall.java

--- Brick_Destroy-master/Brick_Destroy-master/src/test/RubberBall.java
+++ Brick_Destroy-master/Brick_Destroy-master/src/test/RubberBall.java
@@ -1,20 +1,3 @@
-/*
- *  Brick Destroy - A simple Arcade video game
- *   Copyright (C) 2017  Filippo Ranza
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package test;
 
 import java.awt.*;
@@ -37,8 +20,8 @@ public class RubberBall extends Ball {
     @Override
     protected Shape makeBall(Point2D center, int radiusA, int radiusB) {
 
-        double x = center.getX() - (radiusA / 2);
-        double y = center.getY() - (radiusB / 2);
+        double x = center.getX() - (radiusA / 1);
+        double y = center.getY() - (radiusB / 1);
 
         return new Ellipse2D.Double(x,y,radiusA,radiusB);
     }
