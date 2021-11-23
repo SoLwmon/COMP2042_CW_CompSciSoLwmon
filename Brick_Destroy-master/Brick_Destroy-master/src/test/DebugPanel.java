4ee4da0966bdad24f0fe81c327cc531d396da8be
SoLwmon <83406615+SoLwmon@users.noreply.github.com>
2021-11-24 00:19:06 +0800

    Update DebugPanel.java

--- Brick_Destroy-master/Brick_Destroy-master/src/test/DebugPanel.java
+++ Brick_Destroy-master/Brick_Destroy-master/src/test/DebugPanel.java
@@ -1,27 +1,9 @@
-/*
- *  Brick Destroy - A simple Arcade video game
- *   Copyright (C) 2017  Filippo Ranza
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package test;
 
 import javax.swing.*;
 import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionListener;
-
 
 
 public class DebugPanel extends JPanel {
@@ -43,11 +25,9 @@ public class DebugPanel extends JPanel {
 
         initialize();
 
-        skipLevel = makeButton("Skip Level",e -> wall.nextLevel());
-        resetBalls = makeButton("Reset Balls",e -> wall.resetBallCount());
+        addButtons();
 
-        ballXSpeed = makeSlider(-4,4,e -> wall.setBallXSpeed(ballXSpeed.getValue()));
-        ballYSpeed = makeSlider(-4,4,e -> wall.setBallYSpeed(ballYSpeed.getValue()));
+        addSlider();
 
         this.add(skipLevel);
         this.add(resetBalls);
@@ -55,6 +35,18 @@ public class DebugPanel extends JPanel {
         this.add(ballXSpeed);
         this.add(ballYSpeed);
 
+    }
+
+    public void addButtons()
+    {
+        skipLevel = makeButton("Skip Level",e -> wall.nextLevel());
+        resetBalls = makeButton("Reset Balls",e -> wall.resetBallCount());
+    }
+
+    public void addSlider()
+    {
+        ballXSpeed = makeSlider(-4,4,e -> wall.setBallXSpeed(ballXSpeed.getValue()));
+        ballYSpeed = makeSlider(-4,4,e -> wall.setBallYSpeed(ballYSpeed.getValue()));
     }
 
     private void initialize(){
