057c243fadd82b0456be94385f94ec1486f9a306
SoLwmon <83406615+SoLwmon@users.noreply.github.com>
2021-12-04 23:47:45 +0800

    Add files via upload
    to put all constant variable

---
+++ Brick_Destroy-master/Brick_Destroy-master/src/test/Constants.java
@@ -0,0 +1,33 @@
+package test;
+
+import java.awt.*;
+
+public class Constants {
+    public static final String GREETINGS = "Welcome to:";
+    public static final String GAME_TITLE = "Brick Destroy";
+    public static final String CREDITS = "Version 0.1";
+    public static final String START_TEXT = "Start";
+    public static final String MENU_TEXT = "Exit";
+
+    public static final Color BG_COLOR = Color.GREEN.darker();
+    public static final Color BORDER_COLOR = new Color(200,8,21); //Venetian Red
+    public static final Color DASH_BORDER_COLOR = new  Color(255, 216, 0);//school bus yellow
+    public static final Color TEXT_COLOR = new Color(16, 52, 166);//egyptian blue
+    public static final Color CLICKED_BUTTON_COLOR = BG_COLOR.brighter();
+    public static final Color CLICKED_TEXT = Color.WHITE;
+    public static final int BORDER_SIZE = 5;
+    public static final float[] DASHES = {12,6};
+    public static final String CONTINUE = "Continue";
+    public static final String RESTART = "Restart";
+    public static final String EXIT = "Exit";
+    public static final String PAUSE = "Pause Menu";
+    public static final int TEXT_SIZE = 30;
+    public static final Color MENU_COLOR = new Color(0,255,0);
+
+
+    public static final int DEF_WIDTH = 600;
+    public static final int DEF_HEIGHT = 450;
+
+    public static final Color BG_COLOR_Board = Color.WHITE;
+
+}
