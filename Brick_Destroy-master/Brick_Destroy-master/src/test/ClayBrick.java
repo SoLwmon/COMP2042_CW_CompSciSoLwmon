51d34210f43773c755b2b77d8a10b3aa17248ef0
SoLwmon <83406615+SoLwmon@users.noreply.github.com>
2021-12-05 21:37:29 +0800

    Update ClayBrick.java

--- Brick_Destroy-master/Brick_Destroy-master/src/test/ClayBrick.java
+++ Brick_Destroy-master/Brick_Destroy-master/src/test/ClayBrick.java
@@ -1,6 +1,8 @@
 package test;
 
 import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
+import java.awt.geom.RoundRectangle2D.Float;
 
 
 /**
@@ -8,19 +10,21 @@ import java.awt.*;
  *
  */
 public class ClayBrick extends Brick {
-
+	static final Color[] COLOR_INNER =  new Color[] {new Color(0xff0036),new Color(0xfd8317),new Color(0xe3c900), new Color(0x34c61d), new Color(0x34c61d)};
+	static final Color[] COLOR_BORDER =  new Color[] {new Color(0xc00036),new Color(0xd86500),new Color(0xc8af00), new Color(0x26a812), new Color(0x2879c9)};
+	
     private static final String NAME = "Clay Brick";
     private static final Color DEF_INNER = new Color(178, 34, 34).darker();
     private static final Color DEF_BORDER = Color.GRAY;
     private static final int CLAY_STRENGTH = 1;
 
     public ClayBrick(Point point, Dimension size){
-        super(NAME,point,size,DEF_BORDER,DEF_INNER,CLAY_STRENGTH);
+        super(NAME,point,size,COLOR_BORDER[(int)(point.y/size.height)].darker(),COLOR_INNER[(int)(point.y/size.height)],CLAY_STRENGTH);
     }
 
     @Override
     protected Shape makeBrickFace(Point pos, Dimension size) {
-        return new Rectangle(pos,size);
+        return new RoundRectangle2D.Float(pos.x,pos.y,size.width, size.height, 5,5);
     }
 
     @Override
