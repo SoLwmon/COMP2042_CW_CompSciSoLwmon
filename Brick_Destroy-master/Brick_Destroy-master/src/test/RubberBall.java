e70ef2afab7ee53e7c1505aa8011c84f67ef3de2
SoLwmon <83406615+SoLwmon@users.noreply.github.com>
2021-12-05 21:22:30 +0800

    Update RubberBall.java
    change texture of the ball - add ability to the ball

--- Brick_Destroy-master/Brick_Destroy-master/src/test/RubberBall.java
+++ Brick_Destroy-master/Brick_Destroy-master/src/test/RubberBall.java
@@ -8,14 +8,14 @@ public class RubberBall extends Ball {
 
 
     private static final int DEF_RADIUS = 10;
-    private static final Color DEF_INNER_COLOR = new Color(255, 219, 88);
-    private static final Color DEF_BORDER_COLOR = DEF_INNER_COLOR.darker().darker();
-
+    private static final Color DEF_INNER_COLOR = new Color(0xa4fffc);
+    private static final Color DEF_BORDER_COLOR = DEF_INNER_COLOR.darker();
+    private static final Color DEF_INNER_COLOR_SPEED = new Color(0xff4a3c);
+    private static final Color DEF_BORDER_COLOR_SPEED = new Color(0x9f362e);
 
     public RubberBall(Point2D center){
         super(center,DEF_RADIUS,DEF_RADIUS,DEF_INNER_COLOR,DEF_BORDER_COLOR);
     }
-
 
     @Override
     protected Shape makeBall(Point2D center, int radiusA, int radiusB) {
@@ -25,4 +25,21 @@ public class RubberBall extends Ball {
 
         return new Ellipse2D.Double(x,y,radiusA,radiusB);
     }
+
+	@Override
+	public void changeColorState(int state) {
+		// TODO Auto-generated method stub
+		switch(state) {
+		case STATE_NORMAL :
+			this.setBorderColor(DEF_BORDER_COLOR);
+			this.setInnerColor(DEF_INNER_COLOR);
+			break;
+		case STATE_FIRE :
+			this.setBorderColor(DEF_BORDER_COLOR_SPEED);
+			this.setInnerColor(DEF_INNER_COLOR_SPEED);
+			break;
+		}
+	}
+    
+    
 }
